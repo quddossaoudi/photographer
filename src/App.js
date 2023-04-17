5f1f831c27a6b803f8895f849d32fd713838e907
quddossaoudi <quddossaoudi@gmail.com>
2023-04-17 08:09:01 +0800

    intro added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import "./App.css";
-import { useState, CSSProperties } from "react";
+import React, { useEffect, useState } from "react";
 import BarLoader from "react-spinners/BarLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [barWidth, setBarWidth] = useState(300);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setTimeout(() => {
+        setBarWidth(150);
+      }, 7000);
+    }, 8000);
+    setTimeout(() => {
+      setBarWidth(window.innerWidth);
+    }, 7400);
+  }, []);
   return (
-    <div className="App">
-      <p
-        style={{
-          position: "fixed",
-          top: "45%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          zIndex: "9999",
-        }}
-      >
-        Hadiya Ali
-      </p>
-      <BarLoader
-        color="#36d7b7"
-        cssOverride={{
-          position: "fixed",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          zIndex: "9999",
-        }}
-        speedMultiplier={1}
-        width={150}
-      />
+    <div>
+      {loading ? (
+        <>
+          <p
+            style={{
+              position: "fixed",
+              top: "45%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              zIndex: "9999",
+            }}
+          >
+            Hadiya Ali
+          </p>
+          <BarLoader
+            color="#000"
+            cssOverride={{
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              transition: "width 0.5s ease-out",
+            }}
+            speedMultiplier={1}
+            width={barWidth}
+            loading={loading}
+          />
+        </>
+      ) : (
+        <p>hello</p>
+      )}
     </div>
   );
 }
