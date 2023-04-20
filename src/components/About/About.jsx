baad3c687dc997d59c7dafcd8bc186f47b78ca2b
quddossaoudi <quddossaoudi@gmail.com>
2023-04-21 01:36:36 +0800

    cn and about section is-Reveal

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, {useEffect, useRef,useState} from 'react'
 import SectionHeader from '../SectionHeader/index.tsx'
-import css from './About.module.css'
+import useOnScreen from "../hooks/useOnScreen";
+import gsap from "gsap";
+import SplitText from "../utils/Split3.min.js";
+import cn from "classnames";
+import './style.scss'
+
+
 
 function About() {
+  const ref = useRef(null);
+
+  const [reveal, setReveal] = useState(false);
+  const onScreen = useOnScreen(ref);
+
+  useEffect(() => {
+    if (onScreen) setReveal(onScreen);
+  }, [onScreen]);
+
+  useEffect(() => {
+    if (reveal) {
+      const split = new SplitText("#headline", { type: "lines" });
+
+      gsap.to(split.lines, {
+        duration: 1,
+        y: -20,
+        opacity: 1,
+        stagger: 0.1,
+        ease: "power4.out",
+        // onComplete: () => split.revert(),
+      });
+    }
+  }, [reveal]);
+
   return (
-    <section className= {css.aboutSection} data-scroll-section>
-      <SectionHeader title="about"/>
-      <p id="headline">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus distinctio dolorum eveniet ducimus fuga quasi nam beatae tempore facere libero placeat assumenda totam, quo nostrum non laboriosam exercitationem vitae? Dolor! Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem distinctio temporibus necessitatibus perspiciatis ipsam minus eligendi labore quas? Delectus repellendus debitis enim aspernatur dolor deleniti laudantium esse hic dolorum modi.
-      </p>
-    </section>
+    <section
+    className={cn("about-section", { "is-reveal": reveal })}
+    data-scroll-section
+  >
+    <SectionHeader title="about" />
+    <p ref={ref} id="headline" className={cn({ "is-reveal": reveal })}>
+      Flirty Flowers is a blog about flowers and the floral designers who make
+      
+    </p>
+  </section>
   )
 }
 
--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
-import css from './Featured.module.css'
 import photos from '../utils/data'
+import "./style.scss";
+import { useEffect, useRef } from "react";
+
 function Featured() {
-  const [firstUrl, secondUrl] = photos
+  const [firstUrl, secondUrl] = photos;
+  const firstImgRef = useRef(null);
+  const secondImgRef = useRef(null);
+
+  useEffect(() => {
+    const revealImageOnScroll = (ref) => {
+      if (ref.current && ref.current.getBoundingClientRect().top <= window.innerHeight * 0.75) {
+        ref.current.classList.add("is-reveal");
+      }
+    };
+
+    window.addEventListener("scroll", () => {
+      revealImageOnScroll(firstImgRef);
+      revealImageOnScroll(secondImgRef);
+    });
+
+    return () => window.removeEventListener("scroll", revealImageOnScroll);
+  }, []);
+
   return (
-    <section className= {css.featuredSection} data-scroll-section>
-    <div className={css.featuredRowLayout}>
-    <h6>green</h6>
-    <img src ={firstUrl}/>
-   </div>
-  <div className={css.featuredColumnLayout}>
-   <h6>lily</h6>
-   <img src={secondUrl}/>
+    <section className="featured-section" data-scroll-section>
+      <div className="featured-row-layout">
+        <h6>green</h6>
+        <img ref={firstImgRef} src={firstUrl} alt="img" data-scroll />
+      </div>
+      <div className="featured-column-layout">
+        <h6>lily</h6>
+        <img ref={secondImgRef} src={secondUrl} alt="img" data-scroll />
       </div>
     </section>
-  )
+  );
 }
+
 
 export default Featured
